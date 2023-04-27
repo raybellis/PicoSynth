c2f608b86908ecad9231b81a530f980436baa052
Ray Bellis <ray@isc.org>
2023-04-27 12:46:33 +0100

    get the flash serial number for USB device descriptor

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -25,6 +25,7 @@
 
 #include "tusb.h"
 #include "bsp/board.h"
+#include "pico/unique_id.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
@@ -108,10 +109,9 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 //--------------------------------------------------------------------+
 
 // array of pointer to string descriptors
-char const* string_desc_arr [] =
-{
+char const* string_desc_arr [] = {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "Raspberry Pi",                // 1: Manufacturer
+  "Ray Bellis",                  // 1: Manufacturer
   "PicoSynth",                   // 2: Product
   "123456",                      // 3: Serials, should use chip ID
 };
@@ -125,33 +125,45 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
   (void) langid;
 
   uint8_t chr_count;
+  if (index >= sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) return NULL;
 
-  if ( index == 0)
-  {
+  if ( index == 0) {
+
     memcpy(&_desc_str[1], string_desc_arr[0], 2);
     chr_count = 1;
-  }else
-  {
-    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
-    if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
+  } else if (index == 3) {
+
+	const char hex[] = "0123456789abcdef";
+
+	pico_unique_board_id_t board_id;
+    pico_get_unique_board_id(&board_id);
+
+	chr_count = 6;
+	uint8_t idx = PICO_UNIQUE_BOARD_ID_SIZE_BYTES - (chr_count >> 1);
+
+	for (uint8_t i = idx, j = 1; i < PICO_UNIQUE_BOARD_ID_SIZE_BYTES; ++i) {
+		uint8_t b = board_id.id[i];
+		_desc_str[j++] = hex[(b >> 4) & 0x0f];
+		_desc_str[j++] = hex[(b >> 0) & 0x0f];
+	}
+
+  } else {
 
     const char* str = string_desc_arr[index];
 
     // Cap at max char
     chr_count = strlen(str);
-    if ( chr_count > 31 ) chr_count = 31;
+    if (chr_count > 31) chr_count = 31;
 
     // Convert ASCII string into UTF-16
-    for(uint8_t i=0; i<chr_count; i++)
-    {
+    for (uint8_t i = 0; i < chr_count; i++) {
       _desc_str[1+i] = str[i];
     }
   }
 
   // first byte is length (including header), second byte is string type
-  _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+  _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2 * chr_count + 2);
 
   return _desc_str;
 }
